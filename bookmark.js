0ac53e3a7dcf2e0496631643a6fa03ea797fe6f9
Joseph Henry Ilagan <josephhenryilagan@gmail.com>
2026-08-20 00:21:08 +0800

    fix: removed installer button file

--- bookmark.js
+++
@@ -1,6 +0,0 @@
-javascript: (function () {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/sn-toolkit/sn-toolkit-lite@main/sn-toolkit-lite.js';
-    script.type = 'text/javascript';
-    document.head.appendChild(script);
-})();
